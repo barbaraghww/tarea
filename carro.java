96979c46fedf6a5b91b9416ef5370c155a85447a
barbaraghww <139516210+barbaraghww@users.noreply.github.com>
2023-07-13 20:43:44 -0400

    Add files via upload

---
+++ carro.java
@@ -0,0 +1,55 @@
+
+public class carro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class vehiculo {
+
+			int ruedas, asientos, largo, ancho, accesorios, maquinaria;
+			
+			int pesototal;
+			
+			public coche () {
+				ruedas=4;
+				largo=2500;
+				ancho=1000;
+				maquinaria=300;
+				asientos=4;
+				accesorios=10;
+			}
+			public camioneta () {
+					ruedascamion=4;
+					largocamion=3500;
+					anchocamion=1500;
+					maquinariacamion=500;
+					asientoscamion=2;
+					accesorioscamion=20;
+				
+			}
+				public string datosdelvehiculo () {
+					
+					return "El carro posee "+ruedas+", mide de  largo "+largo/1000+" metros y "+ancho/1000" metros de ancho y su maquinaria posee "+maquinaria+" kg de peso, incluye "+asientos+" asientos y si se le agregan accesorios se le agregaria "+accesorios+" kg al peso total del carro""
+							
+							
+				}
+				
+
+				public string datosdelvehiculo () {
+					
+					return "La camioneta posee "+ruedascamion+", mide de  largo "+largocamion/1000+" metros y "+anchocamion/1000" metros de ancho y su maquinaria posee "+maquinariacamion+" kg de peso, incluye "+asientoscamion+" asientos y si se le agregan accesorios se le agregaria "+accesorioscamion+" kg al peso total del carro""
+							
+							
+				}
+				
+				public void marcavehiculo (String marca) {
+					marca=marcavehiculo;
+				
+				
+				}
+
+			}
+
+		}
+	}
+
+}
